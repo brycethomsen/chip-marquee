4ed1881c8f16b3301d3d3587bbcbc766f6dfeb08
Mike Sterry <msterry@spscommerce.com>
2022-05-05 22:57:57 -0500

    feat(colorsFontsModesOhMy): repopulate db, add sql to populate and fix db creation sequence

--- db/schema.sql
+++ db/schema.sql
@@ -29,16 +29,16 @@ CREATE TABLE messages (
 	  FOREIGN KEY (font_id) REFERENCES fonts (font_id) ON DELETE SET DEFAULT
 );
 
-INSERT INTO colors (color_name) VALUES ("AUTOCOLOR");
 INSERT INTO colors (color_name) VALUES ("RED");
+INSERT INTO colors (color_name) VALUES ("AUTOCOLOR");
 INSERT INTO colors (color_name) VALUES ("GREEN");
 INSERT INTO colors (color_name) VALUES ("AMBER");
 INSERT INTO colors (color_name) VALUES ("RAINBOW_1");
 INSERT INTO colors (color_name) VALUES ("RAINBOW_2");
 INSERT INTO colors (color_name) VALUES ("COLOR_MIX");
 
-INSERT INTO modes (mode_name) VALUES ("HOLD");
 INSERT INTO modes (mode_name) VALUES ("ROTATE");
+INSERT INTO modes (mode_name) VALUES ("HOLD");
 INSERT INTO modes (mode_name) VALUES ("ROLL_UP");
 INSERT INTO modes (mode_name) VALUES ("ROLL_DOWN");
 INSERT INTO modes (mode_name) VALUES ("ROLL_LEFT");
@@ -68,6 +68,6 @@ INSERT INTO modes (mode_name) VALUES ("RUNNING_ANIMAL");
 INSERT INTO modes (mode_name) VALUES ("FIREWORKS");
 INSERT INTO modes (mode_name) VALUES ("TURBO_CAR");
 
+INSERT INTO fonts (font_name) VALUES ("FIVE_HIGH_STD");
 INSERT INTO fonts (font_name) VALUES ("SEVEN_HIGH_STD");
 INSERT INTO fonts (font_name) VALUES ("SEVEN_HIGH_FANCY");
-INSERT INTO fonts (font_name) VALUES ("FIVE_HIGH_STD");
